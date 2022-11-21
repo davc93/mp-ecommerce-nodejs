8d551bb8ac2d25b609d4a29d8ed35e3c95017199
Diego Vergara Casanova <davc93@gmail.com>
2022-11-21 14:52:13 -0300

    se cambian a credenciales de testeo

--- config.js
+++ config.js
@@ -2,6 +2,7 @@
 require('dotenv').config()
 const config = {
   ACCESS_TOKEN: process.env.ACCESS_TOKEN,
+  
   GMAIL_APIKEY: process.env.GMAIL_APIKEY,
   payserverDomain:'https://davc93-mp-commerce-nodejs.herokuapp.com'
   
